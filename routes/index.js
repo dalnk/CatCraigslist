b1a98a1c511d19a9fd6920b011fef960bfbf94b2
Dennis Aleynikov <daleynikov@gmail.com>
2018-02-05 13:47:46 -0800

    cats everywhere

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+let model = require('../models/')
 let pets = require('../json/pets')
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('pets-index', { pets: pets });
+  model.Cat.findAll().then(cats => {
+    res.render('cats-index', {cats})
+  })
 });
 
 module.exports = router;
