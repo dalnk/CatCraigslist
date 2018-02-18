cae48e3d62db7a2f2efa5103a7a6960bccd23931
Dennis Aleynikov <daleynikov@gmail.com>
2018-02-18 02:30:00 -0800

    simple search :)

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,43 @@ let pets = require('../json/pets')
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  model.Cat.findAll().then(cats => {
+  query = {}
+  // Check if search term present
+  if(req.query.search) {
+    sanitary = "%" + req.query.search + "%"
+
+    query = {
+      $or: [{name:{$iLike: sanitary}},
+        {species:{$iLike: sanitary}},
+        {bio:{$iLike: sanitary}}]
+    }
+  }
+
+  model.Cat.findAll({
+    where:query
+  }).then((cats) => {
     res.render('cats-index', {cats})
   })
+
 });
+
+router.post('/', (req, res) => {
+  // Check if search term present
+  if(req.query.search) {
+    sanitary = "%" + req.query.search + "%"
+
+    query = {
+      $or: [{name:{$iLike: sanitary}},
+        {species:{$iLike: sanitary}},
+        {bio:{$iLike: sanitary}}]
+    }
+
+    model.Cat.findAll({
+      where:query
+    }).then((cats) => {
+      res.render('cats-index', {cats})
+    })
+  }
+})
 
 module.exports = router;
